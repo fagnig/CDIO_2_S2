eb494c34eb745ba1630048e0880ffe32a89e8748
fagnig <fagnig@autistklassen.dk>
2018-02-26 16:30:01 +0100

    UI reused

---
+++ src/UI/IUI.java
@@ -0,0 +1,8 @@
+package UI;
+
+public interface IUI {
+    public void clearDisplay();
+    public void showMessage(String msg);
+    public int getInteger(String msg);
+    public String getString(String msg);
+}
---
+++ src/UI/TUI.java
@@ -0,0 +1,57 @@
+package UI;
+
+import java.io.Console;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TUI implements IUI {
+
+	Scanner sc = new Scanner(System.in);
+	@Override
+	public void clearDisplay() {
+		System.out.flush();
+
+	}
+
+	@Override
+	public void showMessage(String msg) {
+		System.out.println(msg);
+
+	}
+
+	@Override
+	public int getInteger(String msg) {
+		while(true){
+			try{
+				System.out.println(msg);
+				int i = sc.nextInt();
+				sc.nextLine();
+				return i;
+			}
+			catch(InputMismatchException e){
+				System.out.println("forkert input");
+			}
+			catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+
+
+	}
+
+	@Override
+	public String getString(String msg) {
+		String temp = "";
+		while(true){
+			try{
+				System.out.println(msg);
+				temp = sc.nextLine();
+				return temp;
+			}
+		 	catch(Exception e){
+				e.printStackTrace();
+			}	
+		}
+		
+	}
+}

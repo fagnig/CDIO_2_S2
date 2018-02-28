55abf5c34bfbc721a07c1feb18d3eb049aea65f2
fagnig <fagnig@autistklassen.dk>
2018-02-28 15:04:20 +0100

    First pass on socket implementation

---
+++ src/socket/SocketOutMessage.java
@@ -0,0 +1,14 @@
+package socket;
+
+public class SocketOutMessage {
+    private String message;
+    //TODO implement
+
+    public SocketOutMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
